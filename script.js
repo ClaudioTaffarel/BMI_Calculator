2fdc6c6a3c65b6fde54ede0695aa00adf6b93141
ClaudioTaffarel <claudio.taffarel889@gmail.com>
2025-09-11 23:29:24 +0700

    Commit 4: Menambahkan Design#2

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ btnCalculate.addEventListener("click", () => {
     let height = parseFloat(heightInput.value);
     let weight = parseFloat(weightInput.value);
 
-    if (height == "" || weight == "" || height <= 0 || weight <= 0) {
+    if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
         alert("Please enter valid height and weight!!");
         return;
     }
@@ -35,29 +35,28 @@ btnCalculate.addEventListener("click", () => {
         box.classList.remove("highlight");
     });
 
-    let categoryIndex = -1;
+    let indexxx = -1;
 
     if (bmi < 18.5) {
-        categoryIndex = 0;
+        indexxx = 0;
     } else if (bmi >= 18.5 && bmi < 25) {
-        categoryIndex = 1;
+        indexxx = 1;
     } else if (bmi >= 25 && bmi < 30) {
-        categoryIndex = 2;
+        indexxx = 2;
     } else if (bmi >= 30 && bmi < 35) {
-        categoryIndex = 3;
+        indexxx = 3;
     } else if (bmi >= 35 && bmi < 40) {
-        categoryIndex = 4;
+        indexxx = 4;
     } else if (bmi >= 40) {
-        categoryIndex = 5;
+        indexxx = 5;
     }
 
-    if (categoryIndex !== -1) {
+    if (indexxx !== -1) {
         const boxes = document.querySelectorAll(".bmiBox");
-        boxes[categoryIndex].classList.add("highlight");
-        boxes[categoryIndex].scrollIntoView({ behavior: "smooth", block: "center" });
+        boxes[indexxx].classList.add("highlight");
+        boxes[indexxx].scrollIntoView({ behavior: "smooth", block: "center" });
     }
 });
-
 
 btnReset.addEventListener("click", () => {
     heightInput.value = "";
